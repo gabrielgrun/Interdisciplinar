45e422ac01ecc562d21d91f322d9c0eb309c7088
LucasK <lukretschmer15@gmail.com>
2018-08-15 21:36:34 -0300

    Teste de Erro conexão

--- Ecommerce-web/web/js/indexJS.js
+++ Ecommerce-web/web/js/indexJS.js
@@ -2,8 +2,8 @@
 
 var URL = "http://localhost:8080/ecommerce-web/realizar-compra";
 function registrarCompra(jsonForm) {
-    var pedidoData{};
-    pedidoData["CPEDIDO"] = Number(jsonForm.);
+    var pedidoData = {};
+    pedidoData["CPEDIDO"] = Number(jsonForm);
     var http = new XMLHttpRequest();
     http.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200) {
