e6c114ab2c5183e0d963b714eecbde1f15ad4610
Gabriel Grun <gg005249@cfjl.com.br>
2018-08-23 20:46:07 -0300

    O POLL FUNCIONOU

--- Ecommerce/Ecommerce-ejb/src/java/util/ConnectionUtil.java
+++ Ecommerce/Ecommerce-ejb/src/java/util/ConnectionUtil.java
@@ -1,63 +1,30 @@
 package util;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 
-/**
- *
- * @author Gabriel
- */
 public class ConnectionUtil
 {
 
-    private static Connection connection;
-
     public static Connection getConnection() throws Exception
     {
+        InitialContext ctx;
+        Connection con = null;
 
-        if (connection == null)
-        {
-            System.out.println("Iniciando...");
-
-            try
-            {
-                // Testando o carregamento da classe.
-                Class.forName("org.firebirdsql.jdbc.FBDriver");
-
-                // Dados de cnexão
-                String servidor = "localhost";
-                String dataBase = "D:\\Tecnico Informática\\Interdisciplinar\\ECOMMERCE_BD.FDB";
-                String user = "SYSDBA";
-                String password = "masterkey";
-                // Prepara a URL de conexão
-                // jdbc:forebirdsql:IP/3050:/diretorio/do/banco.fdb?param=adicionais
-                String url = "jdbc:firebirdsql:" + servidor + "/3050:" + dataBase + "?enconding=win1252";
-
-                // Obtem a conexão com o banco de dados
-                connection = DriverManager.getConnection(url, user, password);
-                System.out.println("Conexão estabelecida.");
-            } catch (ClassNotFoundException ex)
-            {
-                throw new Exception(ex);
-            } catch (SQLException ex)
-            {
-                throw new Exception(ex);
-            }
-        }
-
-        return connection;
-    }
-
-    public static void main(String[] args)
-    {
-        System.out.println("Teste");
         try
         {
-            ConnectionUtil.getConnection();
-        } catch (Exception ex)
+            ctx = new InitialContext();
+            DataSource ds = (DataSource) ctx.lookup("jdbc/EcommerceResource");
+
+            con = ds.getConnection();
+        } catch (NamingException | SQLException ex)
         {
-            System.out.println("Erro de SQL");
+            throw new Exception("Erro de conexão ao banco de dados! Verifique o log do aplicativo.", ex);
         }
+
+        return con;
     }
 }
